640f76966431af6f198fe9ad5ab57c9c7f312cee
krishna <sid3238@gmail.com>
2026-06-30 17:05:05 +0530

    Add Terraform GitHub Actions workflow

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "4.79.0"
     }
   }
 }
